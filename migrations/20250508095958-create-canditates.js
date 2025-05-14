a5a45dc41a06584353579fb749d1646ed08a0d16
Meenakshi Sundaram G <127415655+27-MeenakshiSundaram@users.noreply.github.com>
2025-05-14 13:44:18 +0530

    Update 20250508095958-create-canditates.js

--- migrations/20250508095958-create-canditates.js
+++ migrations/20250508095958-create-canditates.js
@@ -1,6 +1,4 @@
 'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('candidates',{
@@ -18,14 +16,7 @@ module.exports = {
       updatedAt:Sequelize.DATE
 
     });
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
   },
-
   async down (queryInterface, Sequelize) {
       await queryInterface.dropTable('candidates'); 
   }
