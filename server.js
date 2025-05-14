5501c95e5fa1a1904f58e85dff46025c2c16c43e
Meenakshi Sundaram G <127415655+27-MeenakshiSundaram@users.noreply.github.com>
2025-05-14 13:46:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/candidates');
-const { sequelize } = require('./models'); // Sequelize init
-
-// Middleware
+const { sequelize } = require('./models'); 
 app.use(express.json());
 
-// Routes
 app.use('/candidates', userRoutes);
-
-// Start the server
 const PORT = 3000;
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
